3ee25183e7e4f4970d65f463aeb3c20298f3b117
ChirimoyaJoyful <81930823+ChirimoyaJoyful@users.noreply.github.com>
2022-05-03 23:55:13 -0400

    Add files via upload

---
+++ grpcClient/index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const redis = require('redis');
+
+var grpc = require("@grpc/grpc-js");
+var protoLoader  = require("@grpc/proto-loader");
+
+
+
+//////////////////////// gRPC
+var PROTO_PATH = "./setup.proto";
+
+var options = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+    arrays: true
+};
+
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+var searchPckg = grpc.loadPackageDefinition(packageDefinition);
+var searchitem = searchPckg.searchPackage;
+var client = new searchitem.SearchItem("server:50051", grpc.credentials.createInsecure());
+
+
+
+
+
+
+//REDIS CONNECTION
+const redisClient = redis.createClient({ url: 'redis://redis-server:6379'});
+redisClient.on('error', (err) => {
+    console.log('An error ocurred', err);
+});
+redisClient.on('connect', (err) => {
+    console.log('Redis connection succesfull');
+});
+redisClient.connect();
+
+
+
+const port = 3000;
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+
+
+
+// app.get('/', async(req,res) => {
+//     pool.query('SELECT Id FROM items', (err,res) =>{
+//         if (err){
+//             console.log(err.stack)
+//         } else{
+//             console.log(res)
+//         }
+//     })
+// });
+
+app.get('/inventory/search', async(req, res) =>{
+    var term = { term: req.query.q };
+    console.log(term);
+    var reply = await redisClient.get(term.term);
+    console.log(reply);
+    if(reply){
+        console.log("Cache");
+        res.send(reply);
+    }else{
+        console.log(term);
+        //var cosas = searchGet(term);
+        client.getSearch(term, async (err, list) =>  {
+            if (err) {
+                console.log('Error al enviar/recibir rpc');
+            } else {
+                console.log(term.term);
+                var cosas = JSON.stringify(list.listEl);
+                console.log(cosas);
+                res.send(cosas);
+                await redisClient.setEx(term.term, 600, cosas)
+            }    
+        });
+        
+    }
+});
+
+//"SELECT * FROM items WHERE Name LIKE '%' || $1 || '%'", [q]
+
+app.listen(port, () =>{
+    console.log('Server running on port', port);
+}); 
---
+++ grpcServer/grpc_server.js
@@ -0,0 +1,128 @@
+var grpc = require("@grpc/grpc-js");
+var protoLoader  = require("@grpc/proto-loader");
+const express = require('express');
+const { Pool } = require('pg');
+
+// POSTGRESQL CONNECTION
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'database',
+    database: 'tiendita',
+    password: 'marihuana',
+    port: 5432,
+});
+
+
+
+var PROTO_PATH = "./setup.proto";
+
+var options = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+    arrays: true
+};
+
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+
+var searchPckg = grpc.loadPackageDefinition(packageDefinition);
+var searchitem = searchPckg.searchPackage;
+
+function getServer() {
+    var server = new grpc.Server();
+    server.addService(searchitem.SearchItem.service,{
+
+        getSearch: getSearch
+
+    });
+    return server;
+}
+
+var routeServer = getServer();
+routeServer.bindAsync("server:50051", grpc.ServerCredentials.createInsecure(), () => {
+    routeServer.start();
+});
+
+
+
+// function searchDB(term) {
+
+    // return new Promise(function (resolve, reject) {
+    //     pool.query("SELECT * FROM items WHERE Name LIKE '%' || $1 || '%'", [term], (err, res) => {
+    //         if (err) {
+    //             console.log('Error saving to db: ' + err);
+    //             return 0;
+    //         } else {
+    //             console.log('id' + res.rows[0]) //This gives me the value to console.
+    //             return res.rows;
+    //         }   
+    //     })
+    // })
+    // var items = pool.query("SELECT * FROM items WHERE Name LIKE '%' || $1 || '%'", [term]);
+    // var cosa = JSON.stringify(items.rows);
+    // console.log(cosa);
+    // return cosa
+// }
+
+
+
+async function getSearch(term,callback) {
+    console.log(term);
+    var termStr = term.request.term;
+    // buscar en base de datos
+    console.log(termStr)
+    // enviar al cliente
+    var items = await pool.query("SELECT * FROM items WHERE Name LIKE '%' || $1 || '%'", [termStr]);
+    var itemJson = JSON.stringify(items.rows);
+    var objList = { listEl : items.rows};
+   // var objList = {listEl: [{"id":5,"name":"John Hardy Womens Legends Naga Gold & Silver Dragon Station Chain Bracelet","price":695,"category":"jewelery","count":400},{"id":6,"name":"Solid Gold Petite Micropave ","price":168,"category":"jewelery","count":70}]}
+    console.log(objList);
+    callback(null, objList);
+    
+}
+
+
+
+
+
+
+
+
+
+// function checkSearch(terminoDeBusqueda) {
+//     console.log("checksearch");
+    
+//     // console.log("Se a enviado un resultado!")
+//     // return results;
+// }
+
+// function getSearch(search, callback) {
+//     console.log("Recibido el termino: ")
+//     console.log(search.request.term);
+//     checkSearch(search.request.term);
+//     callback(null, blank);
+// }
+
+
+
+
+
+
+
+
+// exports.server = server;
+
+const port = 30002;
+
+
+const app = express();
+
+
+app.use(express.json());
+
+app.listen(port, () =>{
+    console.log('Server running on port', port);
+});
